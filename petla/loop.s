63548ecb091abe626dabf9dbb39cc28b8cb71307
Stanislaw Kurzyp <stanislaw1354@gmail.com>
2023-04-18 19:06:29 +0200

    zamiana liter

--- petla/loop.s
+++ petla/loop.s
@@ -1,3 +1,5 @@
+#program drukuje 10 razy liczbe jako kod ascii
+
 STDOUT = 1
 SYSWRITE32 = 4
 EXIT_SUCCESS = 0
@@ -7,18 +9,25 @@ SYSCALL32 = 0x80
 
 .section .data
 number:
-    .long 11234
+    .ascii "11234 \n"
+len = .-number
 
 
 .section .text
 .global _start
 _start:
 
-mov $SYSWRITE32, %eax
-mov $STDOUT, %ebx
-mov $number, %ecx
-mov $4, %edx
-int $SYSCALL32
+xor %esi, %esi
+
+loop:
+    mov $SYSWRITE32, %eax
+    mov $STDOUT, %ebx
+    mov $number, %ecx
+    mov $len, %edx
+    int $SYSCALL32
+    inc %esi
+    cmp $10, %esi
+    jl loop
 
 mov $SYSEXIT32, %eax
 mov $EXIT_SUCCESS, %ebx
---
+++ zamiana_liter/swap.s
@@ -0,0 +1,79 @@
+STDOUT = 1
+STDIN = 0
+SYSWRITE32 = 4
+SYSREAD = 3
+EXIT_SUCCESS = 0
+SYSEXIT32 = 1
+SYSCALL32 = 0x80
+
+.data
+    input: .space 40
+
+.text
+
+    msg: .ascii "Give input: "
+    len = .-msg
+.global _start
+_start:
+    mov $SYSWRITE32, %eax
+    mov $STDOUT, %ebx
+    mov $msg, %ecx
+    mov $len, %edx
+    int $SYSCALL32
+    
+    # pobieranie inputu
+    mov $SYSREAD, %eax
+    mov $STDIN, %ebx
+    mov $input, %ecx
+    mov $40, %edx
+    int $SYSCALL32
+
+    mov %eax, %esi                  # w esi przechochowywana bedzie dlugosc inputu
+    dec %esi                        # iterujemy od 0, dlugosc liczona jest od 1
+
+    #  drukowanie inputu              # do tego momentu to jest echo
+    # mov $SYSWRITE32, %eax
+    # mov $STDOUT, %ebx
+    # mov $input, %ecx
+    # mov $40, %edx
+    # int $SYSCALL32
+
+
+    # petla zamieniajaca male litery na duze
+    xor %edi, %edi                  # edi to licznik petli
+    
+
+    loop:
+        subb $0x20, input(%edi)
+        inc %edi
+        cmp %esi, %edi
+        jl loop
+    # petla zamianiajaca miejscami w parach
+
+    call print
+
+    xor %edi, %edi  
+   
+    push input(%edi)
+    mov $1, %edx                       
+    movb input(%edx,%edi), %ah
+    movb %ah, input(%edi)    
+    pop input(%edx,%edi)                   
+
+    call print
+    
+    # drukowanie inputu              # do tego momentu to jest echo
+    
+
+    mov $SYSEXIT32, %eax
+    mov $EXIT_SUCCESS, %ebx
+    int $SYSCALL32
+
+print:
+    mov $SYSWRITE32, %eax
+    mov $STDOUT, %ebx
+    mov $input, %ecx
+    mov $40, %edx
+    int $SYSCALL32
+    ret
+    
